76000fd77658517c4bc78f5e572268781fd94f6e
Mel Aaron <mel@chromatic.com>
2024-08-23 11:43:52 -0700

    ugh

--- src/components/Shapes.tsx
+++ src/components/Shapes.tsx
@@ -35,7 +35,7 @@ export const Square = (props: Props) => (
   <Shape
     viewBox="0 0 90 90"
     xmlns="http://www.w3.org/2000/svg"
-    color="#FFFFFF"
+    color="#1EA7FD"
     {...props}
     style={{ borderRadius: 5, ...props.style }}
   >

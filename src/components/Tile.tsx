4b5cedd379887579702b6bf607047973964743a1
Mel Aaron <mel@chromatic.com>
2024-09-05 13:43:32 -0700

    change tile backgorund to orange

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -4,7 +4,7 @@ export default styled.div({
   display: 'inline-flex',
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundImage: 'linear-gradient(0deg, #f8f8f8f7 0%, purple 100%)',
+  backgroundImage: 'linear-gradient(0deg, #f8f8f8f7 0%, orange 100%)',
   boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.06)',
   borderRadius: 3,
   padding: '1rem',

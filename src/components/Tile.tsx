ea8da685b54b879b167835ed1b3e8901abd397ca
Mel Aaron <bogus@test.com>
2024-10-22 15:49:03 -0700

    reset border radius

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -6,7 +6,7 @@ export default styled.div({
   alignItems: 'center',
   backgroundImage: 'linear-gradient(0deg, #f8f8f8f7 0%, orange 100%)',
   boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.06)',
-  borderRadius: 13,
+  borderRadius: 3,
   padding: '1rem',
   verticalAlign: 'top',
 });
